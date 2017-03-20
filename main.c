499d9ba0b0f5b26a25c3d1230f07d1d82ce9fcd8
fzm <374648064@qq.com>
2017-03-20 09:42:37 +0800

    创建内核线程

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/sched.h>
+#include <linux/kthread.h>
+#include <linux/err.h>
 #include "common/dateOper.h"
 #include "log/logOper.h"
 #include "common/fileOper.h"
@@ -8,8 +12,18 @@
 #include "running/resource.h"
 
 
+static struct task_struct *monitorTask = NULL;
+
+/**********************************
+ * func: 创建内核线程用于监控资源使用情况
+ * return: void
+ * @para data: 线程参数
+**********************************/
+int monitorResource(void *data);
+
 int Code_init(void)
 {
+	/*
 	char *path = "/proc/2398";
 	
 	char info[8][MAX_INFOLENGTH];
@@ -24,8 +38,18 @@ int Code_init(void)
 		printk("%10s\t", info[i]);
 	}
 	printk("\n");
-	
+	*/
 	printk("success\n");
+	
+	monitorTask = kthread_create(monitorResource, "hello kernel thread", "monitorKthread");
+	if(IS_ERR(monitorTask))
+	{
+		printk("Unable to start kernel thread\n");
+		int err = PTR_ERR(monitorTask);
+		monitorTask = NULL;
+		return err;
+	}
+	wake_up_process(monitorTask);
 	
     return 0;
 }
@@ -33,8 +57,36 @@ int Code_init(void)
 void Code_exit(void)
 {
 	printk("Done!\n");
+	if(monitorTask)
+	{
+		printk("Cancel this kernel thread\n");
+		kthread_stop(monitorTask);
+		printk("Canceled\n");
+	}
     return;
 }
 
 module_init(Code_init);  
-module_exit(Code_exit);  
+module_exit(Code_exit);
+
+int monitorResource(void *data)
+{
+	struct task_struct *task, *p;
+	struct list_head *ps;
+	int count = 0;
+	while(!kthread_should_stop())
+	{
+		task = &init_task;
+		list_for_each(ps, &task->tasks)
+		{
+			p = list_entry(ps, struct task_struct, tasks);
+			count++;
+			printk("%d\t%s\n", p->pid, p->comm);
+		}
+		printk("Process counts: %d\n", count);
+		//printk("data = %s\n", (char *)data);
+		msleep(500);
+	}
+
+	return 0;
+}
